d2b5602dac5b2a76414690eb3eeea7c1c6eea61c
AkbariAidul <akbariaidul@gmail.com>
2025-09-22 16:33:19 +0800

    Membuat dashboard admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\RedirectResponse; // <-- Tambahkan ini
+use Illuminate\Support\Facades\Auth;   // <-- Tambahkan ini
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Tambahkan kode ini
+        RedirectResponse::macro('intended', function ($default = '/', $status = 302, $headers = []) {
+            $path = session()->pull('url.intended', $default);
+
+            if (Auth::check() && Auth::user()->role === 'admin') {
+                // Jika role adalah admin, arahkan ke dashboard admin
+                return redirect()->to('/admin/dashboard', $status, $headers);
+            }
+
+            return redirect()->to($path, $status, $headers);
+        });
     }
 }
